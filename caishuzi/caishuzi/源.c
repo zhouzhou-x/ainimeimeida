0beb321941c00b63ed8e5d4366302eb01e3ecfe9
Zhouzhou <1024303762@qq.com>
2019-11-26 22:18:38 +0800

    增加了次数限制

--- caishuzi/caishuzi/源.c
+++ caishuzi/caishuzi/源.c
@@ -6,27 +6,34 @@ void menu(){
 	printf("******** 欢迎来到猜数字游戏 ********\n");
 	printf("************ 1.play ****************\n");
 	printf("************ 0.exit ****************\n");
-	printf("************************************\n");
+	printf("********** 机会只有3次呦 ************\n");
 }
-void game(){
+void game(){//改进版设置猜数字次数
 	int random_num = rand() % 100 + 1;
 	int input = 0;
-	while (1){
-		printf("请输入你猜的数字:");
-		scanf("%d",&input);
-		if (input > random_num){
-			printf("哥，大了大了\n");
+	int i = 0;
+		while(1){
+			if (i<3){
+				printf("请输入你猜的数字:");
+				scanf("%d", &input);
+				if (input > random_num){
+					printf("哥，大了大了\n");
+				}
+				else if (input < random_num){
+					printf("哥，小了小了\n");
+				}
+				else{
+					printf("猜对了~~~\n");
+					break;
+				}
+			}
+			else {
+				printf("不好意思你失去了3次机会啦，请下次再玩\n");
+				break; }
+			i+=1;
 		}
-		else if (input < random_num){
-			printf("哥，小了小了\n");
-		}
-		else{
-			printf("猜对了~~~");
-			break;
-		}
-
 	}
-}
+
 int main(){
 	int input = 0;
 	do{
