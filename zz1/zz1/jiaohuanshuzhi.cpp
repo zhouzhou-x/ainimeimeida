af66f34412958b2d87b782d72a801246e027f51d
Zhouzhou <1024303762@qq.com>
2019-11-26 21:39:34 +0800

    11.26改进修改

--- zz1/zz1/jiaohuanshuzhi.cpp
+++ zz1/zz1/jiaohuanshuzhi.cpp
@@ -5,13 +5,15 @@ int main(){
 	int a = 0;
 	int b = 0;
 	int c = 0;
-	printf("请输入a，b\n");
-	scanf("%d %d",&a,&b);
+	printf("请输入a=");
+	scanf("%d",&a);
+	printf("请输入b=");
+	scanf("%d", &b);
 	printf("交换前a=%d b=%d\n", a, b);
 	c = a;
 	a = b;
 	b = c;
-	printf("交换后a=%d b=%d", a, b);
+	printf("交换后a=%d b=%d\n", a, b);
 	system("pause");
 	return 0;
 }
--- 找素数/找素数/源.c
+++ 找素数/找素数/源.c
@@ -3,28 +3,12 @@
 #include <stdlib.h>
 int a = 0;
 int i = 0;
-//void zz(){
-//	for (i = 0; i <= a; i++){
-//		if (a%i != 0){
-//			continue;
-//		}
-//		else{
-//			printf("这个数不是素数");
-//			break;
-//		}
-//	}
-//	if (i == a){
-//		printf("这是一个素数");
-//	}
-//}
-void main(){
-	printf("请输入一个数：");
-	scanf("%d", &a);
+void zz(){
 	for (i = 2; i < a; i++){
 		if (a%i != 0){
 			continue;
 		}
-		else if(a%i == 0){  
+		else if (a%i == 0){
 			printf("这个数不是素数\n");
 			break;
 		}
@@ -32,6 +16,11 @@ void main(){
 	if (i == a){
 		printf("这是一个素数\n");
 	}
+}
+void main(){
+	printf("请输入一个数：");
+	scanf("%d", &a);
+	zz();
 	system("pause");
 	return 0;
 }
