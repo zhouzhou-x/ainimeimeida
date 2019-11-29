1ecacbe33c2990621e0eb4f48cff745fc0919ce3
Zhouzhou <1024303762@qq.com>
2019-11-29 23:18:36 +0800

    改进

--- caishuzi/caishuzi/源.c
+++ caishuzi/caishuzi/源.c
@@ -6,14 +6,14 @@ void menu(){
 	printf("******** 欢迎来到猜数字游戏 ********\n");
 	printf("************ 1.play ****************\n");
 	printf("************ 0.exit ****************\n");
-	printf("********** 机会只有3次呦 ************\n");
+	printf("********** 机会只有9次呦 ************\n");
 }
 void game(){//改进版设置猜数字次数
 	int random_num = rand() % 100 + 1;
 	int input = 0;
 	int i = 0;
 		while(1){
-			if (i<3){
+			if (i<9){
 				printf("请输入你猜的数字:");
 				scanf("%d", &input);
 				if (input > random_num){
@@ -32,9 +32,11 @@ void game(){//改进版设置猜数字次数
 				break; }
 			i+=1;
 		}
+		printf("正确的答案是:%d\n", random_num);
 	}
 
 int main(){
+	srand(time(0));//stand((unsigned int)time(NULL))
 	int input = 0;
 	do{
 		menu();
@@ -53,4 +55,4 @@ int main(){
 
 	} while (input);
 	return	0;
-}
+}//写一个新游戏 给4个数字 0~9不会重复 每猜一次 返回信息 信息为位置和数字都对的 和数字对位置不对
